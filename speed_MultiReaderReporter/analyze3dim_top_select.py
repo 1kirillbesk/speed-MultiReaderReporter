20bddd63d2db3e47f2451246093b4747207e1769
oruen321 <runyang.lian@hotmail.de>
2026-04-04 16:45:41 +0200

    ff

--- speed_MultiReaderReporter/analyze3dim_top_select.py
+++ speed_MultiReaderReporter/analyze3dim_top_select.py
@@ -1204,6 +1204,6 @@ def main(dir_path: Path, out_dir: Path):
 if __name__ == "__main__":
     dir_path = Path(r"C:\Users\Public\Documents\RL_project\out_lw\cell_feature")
     out_dir = Path(r"C:\Users\Public\Documents\RL_project\out_lw\feature_plots")  # not used for
-    # dir_path = Path(r"C:\Users\Victus\PycharmProjects\ExpSpeed\out_lw\cell_feature")
-    # out_dir = Path(r"C:\Users\Victus\PycharmProjects\ExpSpeed\out_lw\feature_plots")
+    dir_path = Path(r"C:\Users\Victus\PycharmProjects\ExpSpeed\out_lw\cell_feature")
+    out_dir = Path(r"C:\Users\Victus\PycharmProjects\ExpSpeed\out_lw\feature_plots")
     main(dir_path, out_dir)
